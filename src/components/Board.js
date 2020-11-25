133f1969664515d05a0c73b16bb0728cb938b077
Abayomi <eyitouyoa3@gmail.com>
2020-11-25 10:42:40 -0500

    re-writing the CSS from the ground up

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const Board = ({sounds, playSound, profileName}) =>{
+const Board = ({ sounds, playSound, profileName }) => {
 
-      const soundList = sounds.map(sound =>{
-        return(
-          <div key={sound.id}>
-            <button className="btn" onClick={() => playSound(sound.id)}>{sound.soundName}</button>
-         </div>
-        )
-      })
-  
-      return(
-        <div className="sound-list">
-          <h3>Welcome to the {profileName} page</h3>
-            <div className="btn-container">
-              {soundList}
-            </div>
-        </div>
-      )
-    }
+  const soundList = sounds.map(sound => {
+    return (
+      <div className="btn-container" key={sound.id}>
+        <button className="btn" onClick={() => playSound(sound.id)}>{sound.soundName}</button>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <h3>Welcome to the {profileName} page</h3>
+      <div className="sound-list">
+        {soundList}
+      </div>
+    </>
+  )
+}
 
 
 
