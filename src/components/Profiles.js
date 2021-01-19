496a85f3d8feb88fdc386acfe978fbe561385aa4
Abayomi <eyitouyoa3@gmail.com>
2021-01-19 16:05:43 -0500

    background fill and navigation visuals updated

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -3,10 +3,12 @@ import { Link, withRouter } from 'react-router-dom';
 
 const Profiles = () => {
     return (
-        <div className="profile-routes">
-            <h1>Pick a sound profile from below and enjoy some sweet tunes.</h1>
+        <>
+            <h1>Soundboard</h1>
+            <p>Please select a sound profile from below</p>
+
             <nav>
-                <ul className="profiles">
+                {/* <ul className="profiles">
                     <li className="profile-link"><Link to="/animals">Animals</Link></li>
                     <li className="profile-link"><Link to="/bells">Bells</Link></li>
                     <li className="profile-link"><Link to="/bodily-sounds">Bodily sounds</Link></li>
@@ -17,9 +19,42 @@ const Profiles = () => {
                     <li className="profile-link"><Link to="/office">Office</Link></li>
                     <li className="profile-link"><Link to="/scifi">Scifi</Link></li>
                     <li className="profile-link"><Link to="/war">War</Link></li>
-                </ul>
+                </ul> */}
+
+                <div className="links">
+                    <Link to="/animals" className="profile-link">
+                        <i className='size fas fa-cat'></i>
+                    </Link>
+                    <Link to="/bells" className="profile-link">
+                        <i className='size fas fa-bell'></i>
+                    </Link>
+                    <Link to="/bodily-sounds" className="profile-link">
+                        <i className='size fas fa-user-alt'></i>
+                    </Link>
+                    <Link to="/clocks" className="profile-link">
+                        <i className='size fas fa-clock'></i>
+                    </Link>
+                    <Link to="/construction" className="profile-link">
+                        <i className='size fas fa-hammer'></i>
+                    </Link>
+                    <Link to="/kitchen" className="profile-link">
+                        <i className='size fas fa-blender'></i>
+                    </Link>
+                    <Link to="/music-instruments" className="profile-link">
+                        <i className='size fas fa-guitar'></i>
+                    </Link>
+                    <Link to="/office" className="profile-link">
+                        <i className='size fas fa-building'></i>
+                    </Link>
+                    <Link to="/scifi" className="profile-link">
+                        <i className='size fas fa-atom'></i>
+                    </Link>
+                    <Link to="/war" className="profile-link">
+                        <i className='size fas fa-skull-crossbones'></i>
+                    </Link>
+                </div>
             </nav>
-        </div>
+        </>
 
     )
 }
