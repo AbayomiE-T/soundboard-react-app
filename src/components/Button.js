db53d3f25f9725dd0e331b492ab69672ec1a6648
Abayomi <eyitouyoa3@gmail.com>
2021-01-20 14:34:58 -0500

    fixed a bit of jank

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,32 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
-const Button = ({ playSound, id, soundName, audio }) => {
+const Button = ({ playSound, id, soundName }) => {
 
-    const [clicked, setClick] = useState(false);
-    const iconToggle = useRef();
-
-    const handleClick = () => {
-
-        playSound(id);
-
-        if (clicked) {
-            iconToggle.current.classList.toggle("fa-pause");
-            iconToggle.current.classList.toggle("fa-play");
-            setClick(false);
-        }
-
-        if (!clicked) {
-            iconToggle.current.classList.toggle("fa-play");
-            iconToggle.current.classList.toggle("fa-pause");
-            setClick(true);
-        }
-    }
 
     return (
         <div className="song">
             <span>{soundName}</span>
-            <div onClick={handleClick}>
-                <i ref={iconToggle} className='fas fa-play'></i>
+            <div onClick={() => playSound(id)}>
+                <i className='fas fa-play'></i>
             </div>
         </div>
     )
